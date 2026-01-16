9bb0e6196d5cbb0e1f0698d8494a3736ea551c5c
Juan De Dios Tovar Garcia <jtovgar0504@g.educaand.es>
2026-01-16 10:06:27 +0100

    cambios en casaa

--- src/Hola.java
+++ src/Hola.java
@@ -2,6 +2,6 @@ public class Hola {
     static void main() {
         System.out.println("K");
         System.out.println("Hola");
-        System.out.println("Hola ESPAÑA");
+        System.out.println("Hola ESPAÑAA");
     }
 }
